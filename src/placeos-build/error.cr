3a048dcf8e1f820d883cab09f8433139ee88a896
Caspian Baska <email@caspian.computer>
2021-07-06 21:36:11 +1000

    feat(client): implement a HTTP client

--- src/placeos-build/error.cr
+++ src/placeos-build/error.cr
@@ -1,20 +1,40 @@
-require "exec_from"
+module PlaceOS::Build
+  class Error < Exception
+    def initialize(io : IO, **args)
+      super(io.to_s, **args)
+    end
 
-class PlaceOS::Build::Error < Exception
-  def initialize(io : IO, **args)
-    super(io.to_s, **args)
+    def initialize(message : String?, **args)
+      super
+    end
+
+    class AlreadyCompiling < Error
+    end
+
+    class UnsignedWrite < Error
+      def initialize(**args)
+        @message = "Attempting to write a file to s3 via an unsigned client.\nEnsure the following environment variables are set... AWS_REGION, AWS_KEY, AWS_SECRET, AWS_BUCKET"
+      end
+    end
   end
 
-  def initialize(message : String?, **args)
-    super
-  end
+  class ClientError < Error
+    getter status_code
 
-  class AlreadyCompiling < Error
-  end
+    def initialize(@status_code : Int32, message = "")
+      super(message)
+    end
 
-  class UnsignedWrite < Error
-    def initialize(**args)
-      @message = "Attempting to write a file to s3 via an unsigned client.\nEnsure the following environment variables are set... AWS_REGION, AWS_KEY, AWS_SECRET, AWS_BUCKET"
+    def initialize(path : String, @status_code : Int32, message : String)
+      super("request to #{path} failed with #{message}")
+    end
+
+    def initialize(path : String, @status_code : Int32)
+      super("request to #{path} failed")
+    end
+
+    def self.from_response(path : String, response : HTTP::Client::Response)
+      new(path, response.status_code, response.body)
     end
   end
 end
