9e8ed98501d43030767b188bbcfb87c246f0d33e
Caspian Baska <email@caspian.computer>
2021-07-06 11:41:17 +1000

    refactor(cli): move into seperate files

--- src/cli.cr
+++ src/cli.cr
@@ -3,6 +3,8 @@ require "log"
 
 require "./constants"
 
+require "./cli/*"
+
 module PlaceOS::Build
   def self.run
     if (command = Cli.parse).is_a? Clip::Mapper::Help
@@ -33,10 +35,6 @@ module PlaceOS::Build
     @[Clip::Option("-e", "--env")]
     getter environment : Bool = false
 
-    @[Clip::Doc("Perform a basic health check by requesting the URL")]
-    @[Clip::Option("-c", "--curl")]
-    getter curl : String? = nil
-
     # AWS configuration
     getter aws_region : String? = AWS_REGION
     getter aws_key : String? = AWS_KEY
@@ -55,7 +53,6 @@ module PlaceOS::Build
     def run
       run_version
       run_environment
-      run_curl
     end
 
     protected def run_version
@@ -68,106 +65,6 @@ module PlaceOS::Build
       return unless environment
       ENV.accessed.sort.each &->puts(String)
       exit 0
-    end
-
-    def run_curl
-      return unless url = curl.presence
-      response = HTTP::Client.get url
-      exit 0 if (200..499).includes? response.status_code
-      puts "health check failed, received response code #{response.status_code}"
-      exit 1
-    rescue error
-      error.inspect_with_backtrace(STDOUT)
-      exit 2
-    end
-
-    struct Base < Cli
-    end
-
-    @[Clip::Doc("Run as a REST API, see <TODO: link to openapi.yml>")]
-    struct Server < Cli
-      @[Clip::Doc("Specifies the server host")]
-      getter host : String = "127.0.0.1"
-      @[Clip::Doc("Specifies the server port")]
-      getter port : Int32 = 3000
-
-      @[Clip::Option("-w", "--workers")]
-      @[Clip::Doc("Specifies the number of processes to handle requests")]
-      getter workers : Int32 = 1
-
-      @[Clip::Doc("List the application routes")]
-      @[Clip::Option("-r", "--routes")]
-      getter routes : Bool = false
-
-      def run
-        super
-        run_routes
-
-        # Load the routes
-        puts "Launching #{APP_NAME} v#{VERSION} (#{BUILD_COMMIT} @ #{BUILD_TIME})"
-        server = ActionController::Server.new(port, host)
-
-        # Start clustering
-        server.cluster(workers, "-w", "--workers") if workers > 1
-
-        terminate = Proc(Signal, Nil).new do |signal|
-          puts " > terminating gracefully"
-          spawn(same_thread: true) { server.close }
-          signal.ignore
-        end
-
-        # Detect ctr-c to shutdown gracefully
-        Signal::INT.trap &terminate
-        # Docker containers use the term signal
-        Signal::TERM.trap &terminate
-
-        # Start the server
-        server.run do
-          puts "Listening on #{server.print_addresses}"
-          STDOUT.flush
-        end
-
-        # Shutdown message
-        puts "#{APP_NAME} leaps through the veldt\n"
-      end
-
-      protected def run_routes
-        return unless routes
-        ActionController::Server.print_routes
-        exit 0
-      end
-    end
-
-    @[Clip::Doc("Run as a CLI. Mainly for use in continuous integration.")]
-    struct Build < Cli
-      @[Clip::Argument]
-      getter entrypoints : Array(String)
-
-      def run
-        super
-        abort("Sorry, not implemented yet :(")
-      end
-
-      def drivers
-        self.class.discover_drivers(entrypoints)
-      end
-
-      def self.discover_drivers(paths : Array(String)) : Array(String)
-        paths.uniq.compact_map do |path|
-          if !File.exists?(path)
-            Log.warn { "#{path} is not a file" }
-            path = nil
-          elsif !is_driver?(path)
-            Log.warn { "#{path} is not a driver" }
-            path = nil
-          end
-          path
-        end
-      end
-
-      protected def self.is_driver?(path)
-        !path.ends_with?("_spec.cr") && File.read_lines(path).any? &.includes?("< PlaceOS::Driver")
-      end
     end
   end
 end
---
+++ src/cli/server.cr
@@ -0,0 +1,73 @@
+module PlaceOS::Build
+  abstract struct Cli
+    @[Clip::Doc("Run as a REST API, see <TODO: link to openapi.yml>")]
+    struct Server < Cli
+      @[Clip::Doc("Specifies the server host")]
+      getter host : String = "127.0.0.1"
+      @[Clip::Doc("Specifies the server port")]
+      getter port : Int32 = 3000
+
+      @[Clip::Option("-w", "--workers")]
+      @[Clip::Doc("Specifies the number of processes to handle requests")]
+      getter workers : Int32 = 1
+
+      @[Clip::Doc("List the application routes")]
+      @[Clip::Option("-r", "--routes")]
+      getter routes : Bool = false
+
+      @[Clip::Doc("Perform a basic health check by requesting the URL")]
+      @[Clip::Option("-c", "--curl")]
+      getter curl : String? = nil
+
+      def run
+        super
+        run_routes
+        run_curl
+
+        # Load the routes
+        puts "Launching #{APP_NAME} v#{VERSION} (#{BUILD_COMMIT} @ #{BUILD_TIME})"
+        server = ActionController::Server.new(port, host)
+
+        # Start clustering
+        server.cluster(workers, "-w", "--workers") if workers > 1
+
+        terminate = Proc(Signal, Nil).new do |signal|
+          puts " > terminating gracefully"
+          spawn(same_thread: true) { server.close }
+          signal.ignore
+        end
+
+        # Detect ctr-c to shutdown gracefully
+        Signal::INT.trap &terminate
+        # Docker containers use the term signal
+        Signal::TERM.trap &terminate
+
+        # Start the server
+        server.run do
+          puts "Listening on #{server.print_addresses}"
+          STDOUT.flush
+        end
+
+        # Shutdown message
+        puts "#{APP_NAME} leaps through the veldt\n"
+      end
+
+      protected def run_routes
+        return unless routes
+        ActionController::Server.print_routes
+        exit 0
+      end
+
+      protected def run_curl
+        return unless url = curl.presence
+        response = HTTP::Client.get url
+        exit 0 if (200..499).includes? response.status_code
+        puts "health check failed, received response code #{response.status_code}"
+        exit 1
+      rescue error
+        error.inspect_with_backtrace(STDOUT)
+        exit 2
+      end
+    end
+  end
+end
