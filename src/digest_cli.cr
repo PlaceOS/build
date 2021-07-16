dd321098a3848368f9cd38ae2156bda45a409b57
Caspian Baska <email@caspian.computer>
2021-07-16 17:03:51 +1000

    feat(digest_cli): extract requires

--- src/digest_cli.cr
+++ src/digest_cli.cr
@@ -1,6 +1,8 @@
+require "clip"
 require "compiler/crystal/**"
 require "digest/sha1"
 require "future"
+require "log"
 
 require "placeos-log-backend"
 
@@ -20,157 +22,188 @@ class Crystal::TopLevelVisitor < Crystal::SemanticVisitor
   end
 end
 
-class PlaceOS::Build::Digest::Cli
+module PlaceOS::Build::Digest
   Log = ::Log.for(self)
 
-  def self.run(args = ARGV.dup)
-    paths = [] of Path
+  def self.run
+    if (command = Cli.parse).is_a? Clip::Mapper::Help
+      puts command.help
+      exit 1
+    end
 
-    shard_lock = nil
+    command.run
+  rescue e : Clip::Error
+    puts e
+    exit 1
+  end
 
-    # Command line options
-    OptionParser.parse(args) do |parser|
-      parser.banner = <<-DOC
-      Usage: digest_cli [<crystal entrypoint>]
+  @[Clip::Doc("A program that walks require trees of Crystal programs")]
+  abstract struct Cli
+    include Clip::Mapper
 
-      Outputs a CSV of digested crystal source graphs, formatted as FILE,HASH
-      Expects CRYSTAL_PATH and CRYSTAL_LIBRARY_PATH in the environment\n
-      DOC
+    macro inherited
+      include Clip::Mapper
+      Log = ::Log.for({{ @type }})
+    end
 
-      parser.on("-s PATH", "--shard-lock=PATH", "Specify a shard.lock") do |path|
-        abort("no shard.lock at #{path}") unless File.exists? path
-        shard_lock = path
+    Clip.add_commands({
+      "digest"   => Digest,
+      "requires" => Requires,
+    })
+
+    @[Clip::Doc("Specify a shard.lock")]
+    @[Clip::Option("-s", "--shard-lock")]
+    getter shard_lock : String?
+
+    getter entrypoints : Array(String)
+
+    abstract def run
+
+    CRYSTAL_PATH         = self.validate_env!("CRYSTAL_PATH")
+    CRYSTAL_LIBRARY_PATH = self.validate_env!("CRYSTAL_LIBRARY_PATH")
+
+    protected def self.validate_env!(key)
+      ENV[key]?.presence || abort("#{key} not present in environment")
+    end
+
+    def lock_file
+      shard_lock.try do |f|
+        abort("no shard.lock at #{f}") unless File.exists? f
+        f
+      end
+    end
+
+    def paths
+      entrypoints.compact_map do |f|
+        path = Path[f]
+        if File.exists?(path)
+          path
+        else
+          STDERR.puts "#{path} #{"does not exist.".colorize.red}"
+          nil
+        end
+      end
+    end
+
+    def requires(entrypoint) : Enumerable(String)
+      compiler = ::Crystal::Compiler.new.tap do |config|
+        dev_null = File.open(File::NULL, mode: "w")
+        config.color = false
+        config.no_codegen = true
+        config.release = false
+        config.wants_doc = false
+        config.stderr = dev_null
+        config.stdout = dev_null
       end
 
-      parser.on("-v", "--verbose", "Enable verbose logging") do
-        ::Log.setup("*", :debug, PlaceOS::LogBackend.log_backend)
+      compiler
+        .top_level_semantic(source: ::Crystal::Compiler::Source.new(entrypoint.to_s, File.read(entrypoint)))
+        .program
+        .requires
+    end
+  end
+
+  @[Clip::Doc("Outputs a list of crystal files in a source graphs, one file per line\nExpects CRYSTAL_PATH and CRYSTAL_LIBRARY_PATH in the environment\n")]
+  struct Requires < Cli
+    def run
+      require_channel = Channel(Enumerable(String)).new
+
+      all_paths = paths
+      all_paths.each do |entrypoint|
+        spawn do
+          require_channel.send requires(entrypoint)
+        end
       end
 
-      parser.unknown_args do |pre_dash, post_dash|
-        pre_dash.each.chain(post_dash.each).each do |argument|
-          path = Path[argument]
-          if File.exists?(path)
-            paths << path
-          else
-            STDERR.puts "#{path} #{"does not exist.".colorize.red}"
+      all_paths.size.times do
+        require_channel.receive.each do |f|
+          puts f
+        end
+      end
+    end
+  end
+
+  @[Clip::Doc("Outputs a CSV of digested crystal source graphs, formatted as FILE,HASH\nExpects CRYSTAL_PATH and CRYSTAL_LIBRARY_PATH in the environment\n")]
+  struct Digest < Cli
+    @[Clip::Doc("Enable verbose logging")]
+    @[Clip::Option("-v", "--verbose")]
+    getter verbose : Bool = false
+
+    def run
+      ::Log.setup("*", :debug, PlaceOS::LogBackend.log_backend) if verbose
+
+      lock_hash = lock_file.try &->file_hash(String)
+
+      digests = Channel({Path, String}).new
+      all_paths = paths
+      all_paths.each do |path|
+        spawn do
+          begin
+            before = Time.utc
+            digest = program_hash(path, lock_hash)
+            after = Time.utc
+            Log.trace { "digesting #{path} took #{(after - before).milliseconds}ms" } unless digests.closed?
+            digests.send({path, digest}) rescue nil
+          rescue e
+            Log.error(exception: e) { "failed to digest #{path}" }
+            digests.close
           end
         end
       end
 
-      parser.on("-h", "--help", "Show this help") do
-        puts parser
-        exit 0
+      all_paths.size.times do
+        result = digests.receive?
+        raise "digesting failed!" if result.nil?
+        puts result.join(',')
       end
     end
 
-    new(shard_lock).digest(paths).each { |v| puts v.join(',') }
-  end
+    # Ignore the file if it is from the crystal lib
+    # as `digest_cli` is using the compiler purely to compute requires.
+    protected def crystal_file_hash?(path : String)
+      "c".to_slice if path.starts_with? CRYSTAL_PATH
+    end
 
-  getter shard_digest : Bytes?
+    # TODO:
+    # Look up the SHA1 for a path in the git object store
+    protected def object_store_hash?(path)
+      nil
+    end
 
-  def initialize(shard_lock : String?)
-    # Include the shard.lock (if there is one)
-    if shard_lock
-      @shard_digest = File.open(shard_lock) do |io|
+    protected def digest_hash(path)
+      File.open(path) do |io|
         ::Digest::SHA1.digest &.update(io)
       end
     end
-  end
 
-  def digest(paths : Enumerable(Path))
-    digests = Channel({Path, String}).new
+    def file_hash(path)
+      object_store_hash?(path) || crystal_file_hash?(path) || digest_hash(path)
+    end
 
-    paths.each do |path|
-      spawn do
-        begin
-          before = Time.utc
-          digest = program_hash(path)
-          after = Time.utc
-          Log.trace { "digesting #{path} took #{(after - before).milliseconds}ms" } unless digests.closed?
-          digests.send({path, digest}) rescue nil
-        rescue e
-          Log.error(exception: e) { "failed to digest #{path}" }
-          digests.close
-        end
+    def program_hash(entrypoint : String | Path, shard_digest)
+      # Calculate SHA-1 hash of entrypoint's requires
+      Log.debug { "digesting #{entrypoint}" }
+      futures = requires(entrypoint).map do |file|
+        future {
+          Log.debug { file }
+          file_hash(file)
+        }
       end
+
+      # Include the entrypoint in the hash
+      entrypoint_sha = File.open(entrypoint) do |io|
+        ::Digest::SHA1.digest &.update(io)
+      end
+
+      shas = futures.map &.get
+
+      ::Digest::SHA1.hexdigest do |sha|
+        shas.each { |digest| sha << digest }
+        sha << entrypoint_sha
+        shard_digest.try { |digest| sha << digest }
+      end[0, 6]
     end
-
-    results = [] of {Path, String}
-    paths.size.times do
-      result = digests.receive?
-      raise "digesting failed!" if result.nil?
-      results << result
-    end
-
-    results
-  end
-
-  # Ignore the file if it is from the crystal lib
-  # as `digest_cli` is using the compiler purely to compute requires.
-  protected def crystal_file_hash?(path)
-    "c".to_slice if path.starts_with? CRYSTAL_PATH
-  end
-
-  # TODO:
-  # Look up the SHA1 for a path in the git object store
-  protected def object_store_hash?(path)
-    nil
-  end
-
-  protected def digest_hash(path)
-    File.open(path) do |io|
-      ::Digest::SHA1.digest &.update(io)
-    end
-  end
-
-  def file_hash(path)
-    object_store_hash?(path) || crystal_file_hash?(path) || digest_hash(path)
-  end
-
-  def program_hash(entrypoint : String | Path)
-    compiler = ::Crystal::Compiler.new.tap do |config|
-      dev_null = File.open(File::NULL, mode: "w")
-
-      config.color = false
-      config.no_codegen = true
-      config.release = false
-      config.wants_doc = false
-      config.stderr = dev_null
-      config.stdout = dev_null
-    end
-
-    # Compile and write output to /dev/null (or equivalent)
-    result = compiler.top_level_semantic(source: ::Crystal::Compiler::Source.new(entrypoint.to_s, File.read(entrypoint)))
-
-    # Calculate SHA-1 hash of entrypoint's requires
-    Log.debug { "digesting #{entrypoint}" }
-    futures = result.program.requires.map do |file|
-      future {
-        Log.debug { file }
-        file_hash(file)
-      }
-    end
-
-    # Include the entrypoint in the hash
-    entrypoint_sha = File.open(entrypoint) do |io|
-      ::Digest::SHA1.digest &.update(io)
-    end
-
-    shas = futures.map &.get
-
-    ::Digest::SHA1.hexdigest do |sha|
-      shas.each { |digest| sha << digest }
-      sha << entrypoint_sha
-      shard_digest.try { |digest| sha << digest }
-    end[0, 6]
-  end
-
-  CRYSTAL_PATH         = self.validate_env!("CRYSTAL_PATH")
-  CRYSTAL_LIBRARY_PATH = self.validate_env!("CRYSTAL_LIBRARY_PATH")
-
-  protected def self.validate_env!(key)
-    ENV[key]?.presence || abort("#{key} not present in environment")
   end
 end
 
-PlaceOS::Build::Digest::Cli.run
+PlaceOS::Build::Digest.run
--- src/placeos-build/digest.cr
+++ src/placeos-build/digest.cr
@@ -17,14 +17,33 @@ module PlaceOS::Build::Digest
 
   private EXECUTABLE_PATH = Path["./bin/digest_cli"].expand.to_s
 
+  def self.requires(entrypoints : Array(String), working_directory : String? = nil)
+    output = IO::Memory.new
+    error = IO::Memory.new
+
+    entrypoints = entrypoints.dup
+    entrypoints.unshift("requires")
+    started = Time.utc
+    unless Process.run(EXECUTABLE_PATH, entrypoints, chdir: working_directory, output: output, error: error).success?
+      raise Build::Error.new("failed to extract requires: #{error}")
+    end
+    Log.debug { "extracting requires from #{entrypoints.join(", ")} took #{(Time.utc - started).milliseconds}ms" }
+
+    output
+      .rewind
+      .read_lines(chomp: true)
+  end
+
   # Expects the presence of `digest` binary under `bin` directory
   #
   def self.digest(entrypoints : Array(String), working_directory : String? = nil, shard_lock : String? = nil) : Array(Result)
     output = IO::Memory.new
     error = IO::Memory.new
 
-    started = Time.utc
+    entrypoints = entrypoints.dup
     entrypoints.unshift("-s", shard_lock.as(String)) if shard_lock
+    entrypoints.unshift("digest")
+    started = Time.utc
     unless Process.run(EXECUTABLE_PATH, entrypoints, chdir: working_directory, output: output, error: error).success?
       raise Build::Error.new("failed to digest: #{error}")
     end
