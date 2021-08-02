4e99f1a776bbd0f3a451bb6f3f2e0e1f71d2159b
Caspian Baska <email@caspian.computer>
2021-08-02 12:51:22 +1000

    fix(config): add log filters for sensitive params

--- src/config.cr
+++ src/config.cr
@@ -9,8 +9,10 @@ require "./placeos-build/api/*"
 # Server required after application controllers
 require "action-controller/server"
 
+filters = ["bearer_token", "secret", "password"]
+
 # Add handlers that should run before your application
 ActionController::Server.before(
   ActionController::ErrorHandler.new(PlaceOS::Build.production?, ["X-Request-ID"]),
-  ActionController::LogHandler.new(ms: true)
+  ActionController::LogHandler.new(filters, ms: true)
 )
