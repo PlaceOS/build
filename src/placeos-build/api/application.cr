edb161179427353dd6dca35db091d549f6041613
Caspian Baska <email@caspian.computer>
2021-08-10 13:08:57 +1000

    feat(api): add discover_drivers

--- src/placeos-build/api/application.cr
+++ src/placeos-build/api/application.cr
@@ -10,8 +10,33 @@ module PlaceOS::Build::Api
       Log = ::Log.for({{ @type }})
     end
 
+    # Headers
+    ###########################################################################
+
     getter username : String? { request.headers["X-Git-Username"]?.presence }
     getter password : String? { request.headers["X-Git-Password"]?.presence }
+
+    # Parameters
+    ###########################################################################
+
+    getter repository_uri : String do
+      param url : String, "URL for a git repository"
+    end
+
+    getter branch : String do
+      param branch : String = "master", "Branch to return commits for"
+    end
+
+    getter commit : String do
+      param commit : String = "HEAD", "Commit to checkout"
+    end
+
+    getter repository_path : String? do
+      param repository_path : String? = nil, "Local path to a repository if `build` is configured to support builds referencing a path"
+    end
+
+    # Filters
+    ###########################################################################
 
     before_action :set_request_id
 
