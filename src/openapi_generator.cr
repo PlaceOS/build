379af6221afaf016a5f517a04052f204a80e3618
Kim Burgess <kim@place.technology>
2021-08-13 03:44:24 +1000

    fix: defer API gen until types are known

--- src/openapi_generator.cr
+++ src/openapi_generator.cr
@@ -6,15 +6,17 @@ require "openapi-generator/serializable"
 require "openapi-generator/providers/action-controller"
 require "openapi-generator/helpers/action-controller"
 
-OpenAPI::Generator::Helpers::ActionController.bootstrap
+macro finished
+  OpenAPI::Generator::Helpers::ActionController.bootstrap
 
-OpenAPI::Generator.generate(
-  OpenAPI::Generator::RoutesProvider::ActionController.new,
-  options: {
-    output: Path[Dir.current] / "openapi.yml",
-  },
-  base_document: {
-    info:       {title: "PlaceOS Build", version: PlaceOS::Build::VERSION},
-    components: NamedTuple.new,
-  }
-)
+  OpenAPI::Generator.generate(
+    OpenAPI::Generator::RoutesProvider::ActionController.new,
+    options: {
+      output: Path[Dir.current] / "openapi.yml",
+    },
+    base_document: {
+      info:       {title: "PlaceOS Build", version: PlaceOS::Build::VERSION},
+      components: NamedTuple.new,
+    }
+  )
+end
