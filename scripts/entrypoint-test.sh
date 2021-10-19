3f85b3ab47335dfc7c2089e7787fe6bf77e68458
Caspian Baska <email@caspian.computer>
2021-10-19 12:56:57 +1100

    ci: update test entrypoint

--- scripts/entrypoint-test.sh
+++ scripts/entrypoint-test.sh
@@ -2,29 +2,18 @@
 
 set -eu
 
-# TODO: add once asdf patched for glibc crystal
-# source $HOME/.asdf/asdf.sh
-#
-# asdf install crystal 1.0.0
-# asdf global crystal 1.0.0
-
 if [ -z ${GITHUB_ACTION+x} ]
 then
   echo '### `crystal tool format --check`'
   crystal tool format --check
 
-  echo '### `ameba`' crystal lib/ameba/bin/ameba.cr
+  echo '### `ameba`'
   crystal lib/ameba/bin/ameba.cr
 fi
 
-export CRYSTAL_PATH=lib:/usr/share/crystal/src
-export CRYSTAL_LIBRARY_PATH=/usr/local/lib
-export PKG_CONFIG_PATH=/usr/local/opt/openssl/lib/pkgconfig
-export CPPFlAGS=-L/usr/local/opt/openssl/include
-export LDFLAGS=-L/usr/local/opt/openssl/lib
-
 watch="false"
-multithreaded="false"
+PARAMS=""
+
 while [[ $# -gt 0 ]]
 do
   arg="$1"
@@ -33,11 +22,15 @@ do
     watch="true"
     shift
     ;;
+    *)
+    PARAMS="$PARAMS $1"
+    shift
+    ;;
   esac
 done
 
-if [[ "${watch}" == "true" ]]; then
-  CRYSTAL_WORKERS=$(nproc) watchexec -e cr -c -r -w src -w spec -- scripts/crystal-spec.sh -v "$@"
+if [[ "$watch" == "true" ]]; then
+  CRYSTAL_WORKERS=$(nproc) watchexec -e cr -c -r -w src -w spec -- scripts/crystal-spec.sh -v $PARAMS
 else
-  CRYSTAL_WORKERS=$(nproc) scripts/crystal-spec.sh -v "$@"
+  CRYSTAL_WORKERS=$(nproc) scripts/crystal-spec.sh -v $PARAMS
 fi
