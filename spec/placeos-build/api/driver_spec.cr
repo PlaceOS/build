6d88db684db157ed48492ebe8107068f0a56baa7
Caspian Baska <email@caspian.computer>
2022-07-20 10:57:11 +1000

    refactor(action-controller): use new spec helpers

--- spec/placeos-build/api/driver_spec.cr
+++ spec/placeos-build/api/driver_spec.cr
@@ -5,23 +5,23 @@ module PlaceOS::Build
   describe Api::Driver do
     broken_repository_path = "spec/repository_fixtures/broken"
     broken_entrypoint = "drivers/place/broken.cr"
-    with_server do
-      describe "POST ../:file" do
-        it "gracefully handles malformed driver" do
-          tempdir = Dir.tempdir
-          temporary_repository = File.join(tempdir, Path[broken_repository_path].basename)
-          FileUtils.cp_r(broken_repository_path, tempdir)
-          Build.support_local_builds = true
-          Client.client(URI.parse("http://localhost:6000")) do |client|
-            client.repository_path = temporary_repository
-            client.compile(
-              file: broken_entrypoint,
-              url: "local",
-              commit: "abcde",
-            ) do
-              # This should not be called
-              true.should be_false
-            end
+
+    describe "POST ../:file" do
+      it "gracefully handles malformed driver" do
+        tempdir = Dir.tempdir
+        temporary_repository = File.join(tempdir, Path[broken_repository_path].basename)
+        FileUtils.cp_r(broken_repository_path, tempdir)
+        Build.support_local_builds = true
+
+        Client.client(URI.parse("http://localhost:6000"), connection: ActionController::SpecHelper.client) do |client|
+          client.repository_path = temporary_repository
+          client.compile(
+            file: broken_entrypoint,
+            url: "local",
+            commit: "abcde",
+          ) do
+            # This should not be called
+            true.should be_false
           end
         end
       end
