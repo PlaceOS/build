f778d06979b90a510ad3a5323f9b219049d3e856
Stephen von Takach <steve@place.technology>
2024-04-23 10:54:25 +1000

    chore: crystal tool format

--- src/constants.cr
+++ src/constants.cr
@@ -36,8 +36,8 @@ module PlaceOS::Build
   #############################################################################
 
   # Build Service
-  BUILD_SERVICE_URL = ENV["CLOUD_BUILD_SERVER"]?.presence || "https://build.placeos.run"
-  BUILD_GIT_TOKEN   = ENV["GIT_TOKEN"]?.presence
+  BUILD_SERVICE_URL      = ENV["CLOUD_BUILD_SERVER"]?.presence || "https://build.placeos.run"
+  BUILD_GIT_TOKEN        = ENV["GIT_TOKEN"]?.presence
   BUILD_SERVICE_DISABLED = ENV["BUILD_SERVICE_DISABLED"]? == "true"
 
   class_getter? production : Bool { PRODUCTION }
